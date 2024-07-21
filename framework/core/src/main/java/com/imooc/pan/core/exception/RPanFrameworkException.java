8659d682012425088ad6030a7825b401754f6fef
Gxizai <2046620162@qq.com>
2024-07-22 00:17:44 +0800

    全局异常处理类

---
+++ framework/core/src/main/java/com/imooc/pan/core/exception/RPanFrameworkException.java
@@ -0,0 +1,13 @@
+package com.imooc.pan.core.exception;
+
+/**
+ * @author 囍崽
+ * version 1.0
+ * 技术组件层面的异常对象
+ */
+public class RPanFrameworkException extends RuntimeException {
+
+    public RPanFrameworkException(String message) {
+        super(message);
+    }
+}
