c514f7461bc9f65e500c14502d408911aaa77acd
Gxizai <2046620162@qq.com>
2024-07-22 00:18:08 +0800

    通用响应结果类

---
+++ framework/core/src/main/java/com/imooc/pan/core/response/R.java
@@ -0,0 +1,83 @@
+package com.imooc.pan.core.response;
+
+import com.alibaba.fastjson.annotation.JSONField;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Getter;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ * @author 囍崽
+ * version 1.0
+ * 通用返回对象
+ */
+//保证json序列化的时候，如果属性为null,key也就一起消失
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Getter
+public class R<T> implements Serializable {
+
+    /**
+     * 状态码
+     */
+    private Integer code;
+
+    /**
+     * 状态信息
+     */
+    private String message;
+
+    /**
+     * 返回数据
+     */
+    private T data;
+
+    private R(Integer code) {
+        this.code = code;
+    }
+
+    private R(Integer code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    private R(Integer code, String message, T data) {
+        this.code = code;
+        this.message = message;
+        this.data = data;
+    }
+
+    //在json序列化时将java bean中的一些属性忽略掉，序列化和反序列化都受影响。
+    //一般标记在属性或者方法上，返回的json数据即不包含该属性。
+    @JsonIgnore
+    @JSONField(serialize = false)
+    //该属性不会参与序列化，反之，不加该注解或serialize的值设为true，则会参与序列化。
+    public boolean isSuccess() {
+        return Objects.equals(this.code, ResponseCode.SUCCESS.getCode());
+    }
+
+    public static <T> R<T> success(String message) {
+        return new R<T>(ResponseCode.SUCCESS.getCode(), message);
+    }
+
+    public static <T> R<T> data(T data) {
+        return new R<T>(ResponseCode.SUCCESS.getCode(), ResponseCode.SUCCESS.getDesc(), data);
+    }
+
+    public static <T> R<T> fail() {
+        return new R<T>(ResponseCode.ERROR.getCode());
+    }
+
+    public static <T> R<T> fail(String message) {
+        return new R<T>(ResponseCode.ERROR.getCode(), message);
+    }
+
+    public static <T> R<T> fail(Integer code, String message) {
+        return new R<T>(code, message);
+    }
+
+    public static <T> R<T> fail(ResponseCode responseCode) {
+        return new R<T>(responseCode.getCode(), responseCode.getDesc());
+    }
+}
