7e4ee51c78d08dbae407bf7f94c09f67fe4f26ed
Gxizai <2046620162@qq.com>
2024-08-08 13:49:02 +0800

    文件存储引擎模块开发

---
+++ framework/storage-engine/storage-engine-fastdfs/src/main/java/com/imooc/pan/storage/engine/fd/FastDFSStorageEngine.java
@@ -0,0 +1,11 @@
+package com.imooc.pan.storage.engine.fd;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author AprilGouzi
+ * version 1.0
+ */
+@Component
+public class FastDFSStorageEngine extends AbstractStorageEngine{
+}
---
+++ framework/storage-engine/storage-engine-oss/src/main/java/com/imooc/pan/storage/engine/oss/OSSStorageEngine.java
@@ -0,0 +1,12 @@
+package com.imooc.pan.storage.engine.oss;
+
+import com.imooc.pan.storage.engine.fd.AbstractStorageEngine;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author AprilGouzi
+ * version 1.0
+ */
+@Component
+public class OSSStorageEngine extends AbstractStorageEngine {
+}
